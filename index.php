7f74e12fffa2be750b227184962733d88518dde4
Daniel Mikusa <dmikusa@gopivotal.com>
2014-08-12 09:26:38 -0400

    Link to the log file for easier testing.

--- index.php
+++ index.php
@@ -5,4 +5,4 @@
     $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
     $log->addWarning('some log message');
 ?>
-<p>Check `cf files <app> app/htdocs/app.log`.</p>
+<p>View log file <a href="app.log">here</a>.</p>
